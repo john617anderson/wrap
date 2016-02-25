b54ccb65988d9bee38a3fa755f9fc0d68126f004
John Anderson <johnanderson@Johns-MBP.home>
2016-02-25 10:56:09 -0500

    completed footer

--- wrap/common/footer.php
+++ wrap/common/footer.php
@@ -1,5 +1,13 @@
 <div class="footer">
 	<div class="footerContainer">
+		<div class="smallPrint">
+		©2016 Wrap Media, Inc. All rights reserved.<span>Terms of Service</span><span>Privacy Policy</span><span>Copyright Policy</span>
+		</div>
+		<div class="social">
+			<img src="assets/global/FOOTER/facebook.png" />
+			<img src="assets/global/FOOTER/linkedin.png" />
+			<img src="assets/global/FOOTER/twitter.png" />
+		</div>
 		<div class="column">
 			<ul>
 				<li>home</li>
