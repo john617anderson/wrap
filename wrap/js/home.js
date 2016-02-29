aa810cb67fbd0d3f45d6cae49bf5cd75623179ec
John Anderson <johnanderson@Johns-MBP.home>
2016-02-28 20:18:29 -0500

    carousel

--- wrap/js/home.js
+++ wrap/js/home.js
@@ -3,34 +3,79 @@ $(function(){
 	var _wrapW = 0; 
 	var _w = window.innerWidth;
 	var _remainder;
+	var _center = 2; 
+	var cards = []; 
+	var _big = _w/3.5; 
+	var _r = _big - parseInt(_w/4); 
+	var _start;
 
 	function setWrapWidth(){
 		$('.card').css({width : _w/4}); 
 		_wrapW = $('.card').innerWidth();
+		_start = parseInt(-1*parseInt(_wrapW/2) - _r); 
 
 		$('.card').each(function(i){
+			cards.push($(this));
+
+			if(i === 2){
+				$(this).animate({width : _big, marginTop : -1*_r/1.5})
+			}
+
 			totalWraps++;
 		});
 
-		$('.absoluteContainer').css({width : parseInt(totalWraps*_wrapW)});
+		$('.absoluteContainer').css({width : parseInt(totalWraps*_wrapW) + _w/25, marginLeft : _start});
+
+		console.log(parseInt(-1*parseInt(_w/2)));
 	}
 
 	function createWraps(_total, wrapW){
-		var _containerMargin = 0;  
+		var _containerMargin = _start;  
 		_remainder = parseInt(-1*parseInt(totalWraps*_wrapW - _w) + _wrapW ); 
+
+		$('.caroselContainer').hover(
+			function(){
+				$('.carArrows').animate({opacity : 1}, 300); 
+			},function(){
+				$('.carArrows').animate({opacity : 0}, 300); 
+			}
+		);
 
 		$('.carArrows').click(function(e){
 			var _num; 
+			var _end = false; 
+			var _newCenter; 
+
+			
 
 			if($(this).attr('id') == "left"){
-				if(_containerMargin != 0)
-				_containerMargin = _containerMargin + _wrapW;
+				if(_center != 2){
+					_containerMargin = _containerMargin + _wrapW;
+					_newCenter = _center - 1;				 
+					
+				}else{
+					_end = true;
+				}	
 			}else{
-				 if(_containerMargin > _remainder)
-				_containerMargin = _containerMargin - _wrapW;
+				 if(_center != totalWraps - 3){
+				 	_containerMargin = _containerMargin - _wrapW;
+				 	_newCenter = _center + 1;
+
+				 }else{
+				 	_end = true;
+				 }	
+				
 			}
 
-			$('.absoluteContainer').stop().animate({marginLeft : _containerMargin});
+			if(!_end){
+				cards[_center].stop().animate({marginTop: 0, width : _w/4}, 300, function(){
+					cards[_newCenter].stop().animate({width : _big, marginTop : -1*_r/1.5}, 300);
+					$('.absoluteContainer').stop().animate({marginLeft : _containerMargin});
+					_center = _newCenter
+				});
+
+				
+			}
 		}); 
 	}
 
