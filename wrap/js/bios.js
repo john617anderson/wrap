82f7e265c97840af78c83f52a95c582fc813cef8
John Anderson <johnanderson@Johns-MBP.home>
2016-04-05 16:26:15 -0400

    about modals logic

---
+++ wrap/js/bios.js
@@ -0,0 +1,5 @@
+var bios = [
+	{"name" : "dave cheng", "bio" : "ssdassadasdsadasd"},
+	{"name" : "Mark Rolston", "bio" : "Mark Rolston is a renowned designer with a 25-year career of creating for the world’s largest and most innovative companies. An early pioneer of software user experience, Mark helped forge the disciplines around user interface design and mobile platforms. He is Founder and Chief Creative Officer of Argodesign, which focuses on the modern challenge of design outside the visible artifact. He served previously as Chief Creative Officer of frogdesign, founding the firm’s software business and guiding its transformation into a modern strategic design organization. A veteran design leader, innovator and patent holder, Mark is an internationally recognized expert and valued advisor on software, experience design, and mobile applications."}
+]
+
--- wrap/js/main.js
+++ wrap/js/main.js
@@ -17,15 +17,38 @@ $(function(){
 	$('.modalButton, .close').click(function(){
 		var _this = $(this).attr('id');
 
-		$('.modalContainers').each(function(){
-			($(this).attr('id') === _this) ? $(this).show() : $(this).hide(); 
+		if($(this).attr('class') === "modalButton bios"){
+			var _img = $(this).find('img').attr('src'); 
+			var _name = $(this).find('h4').text(); 
+			var _title = $(this).find('h5').text(); 
 
-			console.log(_this, $(this).attr('id'));
-		});
 
+			_this = $(this).find('h4').text().toLowerCase();  
+
+			for(var i = 0; i < bios.length; i++){
+				var _b = bios[i]; 
+
+				if(_b.name.toLowerCase() === _this){
+					$('.headerBioImage').empty().html('<img src="' + _img + '" />'); 		
+					$('.bio').find('h2').empty().html(_name.toString());
+					$('.bio').find('h3').empty().html(_title.toString());
+					$('.bio').find('p').empty().html(_b.bio.toString());
+
+					console.log(_b.bio.toString());
+				}
+			}
+
+			$('#bioContainer').show(); 
+
+		}else{
+			$('.modalContainers').each(function(){
+				($(this).attr('id') === _this) ? $(this).show() : $(this).hide(); 
+
+				console.log(_this, $(this).attr('id'));
+			});
+		}
 		$('.modal').show(); 
-
-	}); 
+	});
 
 
 	$('.close').click(function(){
